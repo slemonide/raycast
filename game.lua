8d550dbf8b52e47fd8cf5ec87d7eff40d3424003
Danil P <slemonide@gmail.com>
2018-10-27 18:09:54 -0700

    to use map press tab; made light ray display more accurate on the map

--- game.lua
+++ game.lua
@@ -1,6 +1,16 @@
 local game = {}
 
 function game:init()
+    --effect = moonshine(moonshine.effects.vignette)
+    --effect.vignette.radius = 0.8
+    --effect.vignette.softness = 0.9
+    --effect.vignette.opacity = 0.4
+
+    --effect = moonshine(moonshine.effects.desaturate)
+    --effect = moonshine(moonshine.effects.posterize)
+    --effect = moonshine(moonshine.effects.pixelate)
+    --effect = moonshine(moonshine.effects.godsray)
+
     -- 10x10 world
     -- 0 is floor
     -- 1 is wall
@@ -37,9 +47,55 @@ function game:init()
         vz = 0, -- speed in the z direction
         rot = 0
     }
+    game.fluke = {}
+
+    math.randomseed(os.time())
+
+    game.fluke.x = math.random() * CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE
+    game.fluke.y = math.random() * CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE
+
+    angle = math.random() * math.pi * 2
+    game.fluke.vx = CONFIG.FLUKE_SPEED * math.cos(angle)
+    game.fluke.vy = CONFIG.FLUKE_SPEED * math.sin(angle)
+
+    game.staticBurst = love.audio.newSource(love.sound.newSoundData(math.floor(.2 * 44100), 44100, 16, 1))
 end
 
 function game:update(dt)
+    local t1 = os.clock()
+    -- music
+    if not game.staticBurst:isPlaying() then
+        local samples = math.floor(dt*2 * 44100)
+        local data = love.sound.newSoundData(samples, 44100, 16, 1)
+
+        dist = math.sqrt((game.player.x - game.fluke.x)^2 + (game.player.y - game.fluke.y)^2)
+        for i = 1,samples-1 do
+            --[[
+            data:setSample(i, game.player.x/(CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE)*math.sin(0.5*i*game.player.y/(CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE))
+                + game.player.y/(CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE)*math.sin(0.25*i*game.player.x/(CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE)))
+            --]]
+            --data:setSample(i, math.sin(1000/dist*i))
+            data:setSample(i, math.sin(math.pi/2*i)/dist^2*1000)
+
+        end
+        game.staticBurst = love.audio.newSource(data)
+        --love.audio.play(game.staticBurst)
+    end
+
+    -- fluke
+    game.fluke.x = game.fluke.x + game.fluke.vx * dt
+    game.fluke.y = game.fluke.y + game.fluke.vy * dt
+
+    if game.fluke.x < 0 or game.fluke.x > CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE then
+        game.fluke.vx = -game.fluke.vx
+        game.fluke.x = game.fluke.x + game.fluke.vx * dt
+    end
+    if game.fluke.y < 0 or game.fluke.y > CONFIG.NODE_SIZE * CONFIG.WORLD_SIZE then
+        game.fluke.vy = -game.fluke.vy
+        game.fluke.y = game.fluke.y + game.fluke.vy * dt
+    end
+
+    -- player
     local last_pos = {
         x = game.player.x,
         y = game.player.y
@@ -64,8 +120,8 @@ function game:update(dt)
 
     -- collision detection
     if game:isWall(game.player) then
-        --game.player.x = last_pos.x
-        --game.player.y = last_pos.y
+        game.player.x = last_pos.x
+        game.player.y = last_pos.y
     end
 
     if love.keyboard.isDown("a") then
@@ -93,6 +149,14 @@ end
 
 function game:draw()
     local t1 = os.clock()
+    function sine(f,t)
+        return (math.sin(f*t/500)+1)/2
+        --return (math.sin(f*t/100)+1)/2
+    end
+
+    --effect.vignette.color = {255*sine(math.sqrt(31),t1*50),255*sine(math.sqrt(21),t1*50),255*sine(math.sqrt(5),t1*50)}
+
+    --effect(function()
 
     local w = love.graphics.getWidth()
     local h = love.graphics.getHeight()
@@ -106,15 +170,11 @@ function game:draw()
 
     -- draw walls
 
-    function sine(f,t)
-        return (math.sin(f*t/500)+1)/2
-    end
-
     -- render scene
     for i=1,w do
         local rot = game.player.rot + (i - w/2) * CONFIG.FOV/w
 
-        local dist, side = game:getDistanceToObstacle(rot)
+        local dist, side = game:getDistanceToObstacle(rot, true)
         local shadow = math.min(dist/CONFIG.SHADOW_SIZE,0.5)
         --[[if side == "x" then
             love.graphics.setColor(0.6-shadow,0.6-shadow,0.6-shadow)
@@ -124,16 +184,32 @@ function game:draw()
         --]]
         --[[
         if side == "x" then
-            if i*math.floor(t1*10) % 10 <= 5 then
+            if i*math.floor(t1*100) % 10 <= 5 then
                 love.graphics.setColor(0,0,1-math.min(dist/350,1))
             else--if i % 3 == 1 then
                 love.graphics.setColor(1-math.min(dist/350,1),0,0)
             end
         elseif side == "y" then
-            if i*math.floor(t1*20) % 10 <= 5 then
+            if i*math.floor(t1*200) % 10 <= 5 then
                 love.graphics.setColor(0,0,math.min(dist/350,1))
             else--if i % 3 == 1 then
                 love.graphics.setColor(math.min(dist/350,1),0,0)
+            end
+            --love.graphics.setColor(0, 1-math.min(dist/350,1),0)
+        end
+        --]]
+        --[[
+        if side == "x" then
+            if i*math.floor(t1*5) % 10 <= 5 then
+                love.graphics.setColor((0.6-shadow)*sine(math.sqrt(2),t1),(0.6-shadow)*sine(math.exp(1),t1),1-math.min(dist/350,1))
+            else--if i % 3 == 1 then
+                love.graphics.setColor(1-math.min(dist/350,1),(0.6-shadow)*sine(math.sqrt(7),t1),(0.6-shadow)*sine(math.exp(13),t1))
+            end
+        elseif side == "y" then
+            if i*math.floor(t1*3) % 10 <= 5 then
+                love.graphics.setColor((0.6-shadow)*sine(math.sqrt(32),t1),(0.6-shadow)*sine(math.exp(31),t1),math.min(dist/350,1))
+            else--if i % 3 == 1 then
+                love.graphics.setColor(math.min(dist/350,1),(0.6-shadow)*sine(math.sqrt(5),t1),(0.6-shadow)*sine(math.exp(11),t1))
             end
             --love.graphics.setColor(0, 1-math.min(dist/350,1),0)
         end
@@ -145,66 +221,109 @@ function game:draw()
             love.graphics.setColor((0.55-shadow)*sine(7,t1), (0.55-shadow)*sine(math.pi*2,t1),(0.55-shadow)*sine(13,t1))
         end
         --]]
-        ----[[
+        --[[
         if side == "x" then
-            love.graphics.setColor((0.6-shadow)*sine(math.sqrt(2),game.player.x),
-                (0.6-shadow)*sine(math.exp(1),game.player.y),
-                (0.6-shadow)*sine(math.sqrt(13),game.player.x))
+            love.graphics.setColor((0.6-shadow)*sine(math.sqrt(2),game.player.x)*sine(math.sqrt(5),t1),
+                (0.6-shadow)*sine(math.exp(1)*sine(math.sqrt(7),t1),game.player.y),
+                (0.6-shadow)*sine(math.sqrt(13)*sine(math.sqrt(11),t1),game.player.x))
         elseif side == "y" then
-            love.graphics.setColor((0.55-shadow)*sine(7,game.player.y),
-                (0.55-shadow)*sine(math.pi*2,game.player.x),
-                (0.55-shadow)*sine(13,game.player.y))
+            love.graphics.setColor((0.55-shadow)*sine(7,game.player.y)*sine(math.sqrt(31),t1),
+                (0.55-shadow)*sine(math.pi*2,game.player.x)*sine(math.sqrt(13),t1),
+                (0.55-shadow)*sine(13,game.player.y)*sine(math.sqrt(math.pi),t1))
+        end
+        --]]
+        ----[[
+        shadow = shadow*4
+        if side == "x" then
+            love.graphics.setColor(math.exp(-shadow)*sine(math.sqrt(2),game.player.x)*sine(math.sqrt(5),t1),
+                math.exp(-shadow)*sine(math.exp(1)*sine(math.sqrt(7),t1),game.player.y),
+                math.exp(-shadow)*sine(math.sqrt(13)*sine(math.sqrt(11),t1),game.player.x))
+        elseif side == "y" then
+            love.graphics.setColor(math.exp(-shadow)*sine(7,game.player.y)*sine(math.sqrt(31),t1),
+                math.exp(-shadow)*sine(math.pi*2,game.player.x)*sine(math.sqrt(13),t1),
+                math.exp(-shadow)*sine(13,game.player.y)*sine(math.sqrt(math.pi),t1))
         end
         --]]
         love.graphics.line(i, game.player.z + h/2 - 10000/dist, i, game.player.z + h/2 + 10000/dist)
+        --[[
+        for z=math.floor(game.player.z + h/2 - 10000/dist), math.floor(game.player.z + h/2 + 10000/dist) do
+            shadow = shadow
+            shadow = math.sqrt((shadow/100)^2 + (z/300)^2)
+            if side == "x" then
+                love.graphics.setColor(math.exp(-shadow)*sine(math.sqrt(2),game.player.x)*sine(math.sqrt(5),t1),
+                    math.exp(-shadow)*sine(math.exp(1)*sine(math.sqrt(7),t1),game.player.y),
+                    math.exp(-shadow)*sine(math.sqrt(13)*sine(math.sqrt(11),t1),game.player.x))
+            elseif side == "y" then
+                love.graphics.setColor(math.exp(-shadow)*sine(7,game.player.y)*sine(math.sqrt(31),t1),
+                    math.exp(-shadow)*sine(math.pi*2,game.player.x)*sine(math.sqrt(13),t1),
+                    math.exp(-shadow)*sine(13,game.player.y)*sine(math.sqrt(math.pi),t1))
+            end
+
+            love.graphics.points(i, z)
+        end
+        --]]
     end
 
     -- render map
-    --[[
-    for i,v in ipairs(game.world) do
-        x = ((i - 1) % CONFIG.WORLD_SIZE) * CONFIG.NODE_SIZE
-        y = (math.floor((i - 1) / CONFIG.WORLD_SIZE)) * CONFIG.NODE_SIZE
-        if v == 1 then
-            love.graphics.setColor(0.70, 0.63, 0.05)
-            love.graphics.rectangle("fill", x, y, CONFIG.NODE_SIZE, CONFIG.NODE_SIZE)
+    if love.keyboard.isDown("tab") then
+        for i,v in ipairs(game.world) do
+            x = ((i - 1) % CONFIG.WORLD_SIZE) * CONFIG.NODE_SIZE
+            y = (math.floor((i - 1) / CONFIG.WORLD_SIZE)) * CONFIG.NODE_SIZE
+            if v == 1 then
+                love.graphics.setColor(0.70, 0.63, 0.05)
+                love.graphics.rectangle("fill", x, y, CONFIG.NODE_SIZE, CONFIG.NODE_SIZE)
+            end
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.rectangle("line", x, y, CONFIG.NODE_SIZE, CONFIG.NODE_SIZE)
         end
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.rectangle("line", x, y, CONFIG.NODE_SIZE, CONFIG.NODE_SIZE)
+
+
+        love.graphics.setColor(1, 0.42, 0.64)
+        love.graphics.arc("fill", game.player.x, game.player.y,
+            CONFIG.FOV_TRIANGLE_SIZE,
+            game.player.rot + CONFIG.FOV/2,
+            game.player.rot - CONFIG.FOV/2)
+        love.graphics.setColor(0.42, 0.63, 0.05)
+        love.graphics.circle("fill", game.player.x, game.player.y, CONFIG.NODE_SIZE/4)
+
+        love.graphics.setColor(0, 1, 0)
+        for i=-50,50 do
+            local rot = game.player.rot + i * CONFIG.FOV/100
+
+            dist, side, points = game:getDistanceToObstacle(rot, true)
+            love.graphics.line(points)
+            --[[
+            love.graphics.line(game.player.x, game.player.y,
+                game.player.x + math.cos(rot) * dist,
+                game.player.y + math.sin(rot) * dist)
+            --]]
+        end
+
+        love.graphics.setColor(0.82, 0.63, 0.05)
+        love.graphics.circle("fill", game.fluke.x, game.fluke.y, CONFIG.NODE_SIZE/4)
+
+        --]]
+        local t2 = os.clock()
+        local fps = string.format("FPS: %.0f", 1/(t2 - t1))
+        love.graphics.print(fps, w - 100, 10)
     end
-
-
-    love.graphics.setColor(1, 0.42, 0.64)
-    love.graphics.arc("fill", game.player.x, game.player.y,
-        CONFIG.FOV_TRIANGLE_SIZE,
-        game.player.rot + CONFIG.FOV/2,
-        game.player.rot - CONFIG.FOV/2)
-    love.graphics.setColor(0.42, 0.63, 0.05)
-    love.graphics.circle("fill", game.player.x, game.player.y, CONFIG.NODE_SIZE/4)
-
-    love.graphics.setColor(0, 1, 0)
-    for i=-50,50 do
-        local rot = game.player.rot + i * CONFIG.FOV/100
-
-        dist = game:getDistanceToObstacle(rot)
-        love.graphics.line(game.player.x, game.player.y,
-            game.player.x + math.cos(rot) * dist,
-            game.player.y + math.sin(rot) * dist)
-    end
-    --]]
-    local t2 = os.clock()
-    local fps = string.format("FPS: %.0f", 1/(t2 - t1))
-    love.graphics.print(fps, w - 100, 10)
+    --end)
 end
 
-function game:getDistanceToObstacle(angle)
+function game:getDistanceToObstacle(angle,bend)
     local distance_so_far = 0
+
+    local points = {}
 
     local current_pos = {
         x = game.player.x,
         y = game.player.y
     }
 
-    local step = 0.5
+    table.insert(points, current_pos.x)
+    table.insert(points, current_pos.y)
+
+    local step = 1
     local dp = {
         x = step * math.cos(angle),
         y = step * math.sin(angle)
@@ -222,25 +341,45 @@ function game:getDistanceToObstacle(angle)
  
             if CONFIG.FISH_EYE_CORRECTION then
                 if math.tan(angle_in) > 0 then
-                    return distance_so_far * math.cos((angle - game.player.rot) * CONFIG.FISH_EYE_FACTOR), "x"
+                    return distance_so_far * math.cos((angle - game.player.rot) * CONFIG.FISH_EYE_FACTOR), "x", points
                 else
-                    return distance_so_far * math.cos((angle - game.player.rot) * CONFIG.FISH_EYE_FACTOR), "y"
+                    return distance_so_far * math.cos((angle - game.player.rot) * CONFIG.FISH_EYE_FACTOR), "y", points
                 end
             else
                 if math.tan(angle_in) > 0 then
-                    return distance_so_far, "x"
+                    return distance_so_far, "x", points
                 else
-                    return distance_so_far, "y"
+                    return distance_so_far, "y", points
                 end
             end
         else
-            current_pos.x = current_pos.x + dp.x
-            current_pos.y = current_pos.y + dp.y
-            distance_so_far = distance_so_far + step
+            inv_fac = math.sqrt((game.fluke.x - current_pos.x)^2 + (game.fluke.y - current_pos.y)^2)
+
+            a = {
+                x = CONFIG.FLUKE_STRENGTH * (game.fluke.x - current_pos.x)/inv_fac,
+                y = CONFIG.FLUKE_STRENGTH * (game.fluke.x - current_pos.x)/inv_fac
+            }
+
+            if bend then
+                dp.x = dp.x + a.x * 0.1
+                dp.y = dp.y + a.y * 0.1
+            end
+
+            mag = math.sqrt(dp.x^2 + dp.y^2)
+
+            dp.x = dp.x / mag * step
+            dp.y = dp.y / mag * step
+
+            current_pos.x = current_pos.x + dp.x * 0.8
+            current_pos.y = current_pos.y + dp.y * 0.8
+            distance_so_far = distance_so_far + step * 0.8
+
+            table.insert(points, current_pos.x)
+            table.insert(points, current_pos.y)
         end
     end
 
-    return distance_so_far, "x"
+    return distance_so_far, "x", points
 end
 
 --[[
--- globals.lua
+++ globals.lua
@@ -7,6 +7,7 @@
 Class = require "lib.hump.class"
 Gamestate = require "lib.hump.gamestate"
 suit = require 'lib.suit'
+moonshine = require 'lib.moonshine'
 
 -- Load global objects
 states = {}
@@ -28,9 +29,12 @@ CONFIG = {
     JUMP_SPEED = 200,
     GRAV_ACC = -200,
     -- 4 * math.pi
-    FOV = math.pi*2,
+    FOV = math.pi,
     FOV_TRIANGLE_SIZE = 40,
     FOV_SPEED = 100 / 180 * math.pi,
     FISH_EYE_CORRECTION = false,
-    FISH_EYE_FACTOR = 0.88
+    FISH_EYE_FACTOR = 0.88,
+
+    FLUKE_SPEED = 5,--100,
+    FLUKE_STRENGTH = -0.1--0.3 --0.1
 }
